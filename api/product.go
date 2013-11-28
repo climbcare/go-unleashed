0ccc894b8bccdb4607551293892696ffc36cbb4a
Jonathan Sokolowski <jonathan.sokolowski@gmail.com>
2013-11-29 09:02:15 +1100

    Add a basic product struct and make JSON unmarshalling work

---
+++ api/product.go
@@ -0,0 +1,134 @@
+package api
+
+import (
+    "errors"
+    "fmt"
+    "regexp"
+    "strconv"
+
+    "github.com/nu7hatch/gouuid"
+)
+
+type Guid struct {
+    guid *uuid.UUID
+}
+
+func (g *Guid) UnmarshalJSON(value []byte) (err error) {
+    buffer := string(value[1 : len(value) - 1])  // value is quoted
+    g.guid, err = uuid.ParseHex(buffer)
+    return
+}
+
+func (g *Guid) String() string {
+    if g.guid != nil {
+        return g.guid.String()
+    } else {
+        return "nil"
+    }
+}
+
+type DateTime struct {
+    ms int64
+}
+
+func (dt *DateTime) UnmarshalJSON(value []byte) error {
+    s := string(value[1 : len(value) - 1])
+    fmt.Println(s)
+
+    re := regexp.MustCompile("^\\/Date\\((-?[0-9]+)\\)\\/")
+    md := re.FindStringSubmatch(s)
+    if md == nil {
+        return errors.New("Invalid DateTime string")
+    }
+
+    if ms, err := strconv.ParseInt(md[1], 10, 0); err != nil {
+        return errors.New("Could not Parse DateTime string")
+    } else {
+        dt.ms = ms
+    }
+
+    return nil
+}
+
+type Product struct {
+      AverageLandPrice float32
+      Barcode string
+      BinLocation string
+      CanAutoAssemble bool
+      CreatedBy string
+      DefaultPurchasePrice float32
+      DefaultSellPrice float32
+      Depth float32
+      Guid Guid
+      Height float32
+      IsAssembledProduct bool
+      IsComponent bool
+      LastCost float32
+      LastModifiedOn DateTime
+      MaxStockAlertLevel float32
+      MinStockAlertLevel float32
+      NeverDiminishing bool
+      Notes string
+      Obsolete bool
+      PackSize float32
+      ProductCode string
+      ProductDescription string
+      ProductGroup ProductGroup
+      ReOrderPoint float32
+      SellPriceTier1 SellPriceTier
+      SellPriceTier2 SellPriceTier
+      SellPriceTier3 SellPriceTier
+      SellPriceTier4 SellPriceTier
+      SellPriceTier5 SellPriceTier
+      SellPriceTier6 SellPriceTier
+      SellPriceTier7 SellPriceTier
+      SellPriceTier8 SellPriceTier
+      SellPriceTier9 SellPriceTier
+      SellPriceTier10 SellPriceTier
+      SourceId string
+      SourceVariantParentId string
+      Supplier Supplier
+      Taxable bool
+      TaxableSales bool
+      UnitOfMeasure UnitOfMeasure
+      Weight float32
+      Width float32
+      XeroSalesAccount string
+      XeroSalesTaxCode string
+      XeroSalesTaxRate float32
+      XeroTaxCode string
+      XeroTaxRate float32
+}
+
+type ProductGroup struct {
+    Guid      *Guid
+    GroupName string
+}
+
+func (pg ProductGroup) String() string {
+    return fmt.Sprintf("{ ProductGroup: %s %s }", pg.Guid, pg.GroupName)
+}
+
+type SellPriceTier struct {
+    Name  string
+    Value string
+}
+
+type Supplier struct {
+    Guid         *Guid
+    SupplierCode string
+    SupplierName string
+}
+
+func (s Supplier) String() string {
+    return fmt.Sprintf("{ Supplier: %s %s %s }", s.Guid, s.SupplierCode, s.SupplierName)
+}
+
+type UnitOfMeasure struct {
+    Guid *uuid.UUID
+    Name string
+}
+
+func (um UnitOfMeasure) String() string {
+    return fmt.Sprintf("{ UnitOfMeasure: %s %s }", um.Guid, um.Name)
+}
---
+++ api/product_test.go
@@ -0,0 +1,130 @@
+package api
+
+import (
+    . "launchpad.net/gocheck"
+
+    "encoding/json"
+    "log"
+)
+
+var _ = log.Println
+
+
+type ProductSuite struct{}
+var _ = Suite(&ProductSuite{})
+
+func (s *ProductSuite) TestGuid(c *C) {
+    var guid Guid
+    err := json.Unmarshal([]byte(`"76b4e5d4-ff42-4785-93c5-a69a2980752d"`), &guid)
+
+    c.Assert(err, IsNil)
+}
+
+func (s *ProductSuite) TestDateTime(c *C) {
+    var dt DateTime
+    err := json.Unmarshal([]byte(`"/Date(1378686019420)/"`), &dt)
+
+    c.Assert(err, IsNil)
+    c.Assert(dt.ms, Equals, int64(1378686019420))
+}
+
+func (s *ProductSuite) TestProduct(c *C) {
+    var p Product
+    err := json.Unmarshal(productExample(), &p)
+
+    c.Assert(err, IsNil)
+
+    c.Assert(p.ProductCode, Equals, "_TEST01")
+    c.Assert(p.SellPriceTier1.Name, Equals, "RRP")
+    c.Assert(p.SellPriceTier1.Value, Equals, "15.0000")
+
+    log.Println(p.ProductGroup)
+}
+
+
+func productExample() []byte {
+    p := []byte(`{
+        "ProductCode": "_TEST01",
+        "ProductDescription": "Test Item 01",
+        "Barcode": null,
+        "PackSize": null,
+        "Width": null,
+        "Height": null,
+        "Depth": null,
+        "Weight": null,
+        "MinStockAlertLevel": null,
+        "MaxStockAlertLevel": null,
+        "ReOrderPoint": null,
+        "UnitOfMeasure": null,
+        "NeverDiminishing": false,
+        "LastCost": null,
+        "DefaultPurchasePrice": 5,
+        "DefaultSellPrice": 10,
+        "AverageLandPrice": null,
+        "Obsolete": false,
+        "Notes": null,
+        "SellPriceTier1": {
+            "Name": "RRP",
+            "Value": "15.0000"
+        },
+        "SellPriceTier2": {
+            "Name": "",
+            "Value": "0.0000"
+        },
+        "SellPriceTier3": {
+            "Name": "Sell Price Tier 3",
+            "Value": "0.0000"
+        },
+        "SellPriceTier4": {
+            "Name": "Sell Price Tier 4",
+            "Value": "0.0000"
+        },
+        "SellPriceTier5": {
+            "Name": "Sell Price Tier 5",
+            "Value": "0.0000"
+        },
+        "SellPriceTier6": {
+            "Name": "Sell Price Tier 6",
+            "Value": "0.0000"
+        },
+        "SellPriceTier7": {
+            "Name": "Sell Price Tier 7",
+            "Value": "0.0000"
+        },
+        "SellPriceTier8": {
+            "Name": "Sell Price Tier 8",
+            "Value": "0.0000"
+        },
+        "SellPriceTier9": {
+            "Name": "Sell Price Tier 9",
+            "Value": "0.0000"
+        },
+        "SellPriceTier10": {
+            "Name": "Sell Price Tier 10",
+            "Value": "0.0000"
+        },
+        "Taxable": false,
+        "XeroTaxCode": "EXEMPTEXPENSES",
+        "XeroTaxRate": 0,
+        "TaxableSales": true,
+        "XeroSalesTaxCode": "OUTPUT",
+        "XeroSalesTaxRate": 0.1,
+        "IsComponent": false,
+        "IsAssembledProduct": false,
+        "CanAutoAssemble": false,
+        "ProductGroup": {
+            "Guid": "e76c9496-1571-4ee7-bf75-334ccbf45d29",
+            "GroupName": "_Test"
+        },
+        "XeroSalesAccount": null,
+        "BinLocation": null,
+        "Supplier": null,
+        "SourceId": null,
+        "CreatedBy": "admin@example.com",
+        "SourceVariantParentId": null,
+        "Guid": "76b4e5d4-ff42-4785-93c5-a69a2980752d",
+        "LastModifiedOn": "/Date(1378686019420)/"
+    }`)
+
+    return p
+}
